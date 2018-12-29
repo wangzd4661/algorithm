4588487b2cf7bce7f6d75cbe44c24cddb74e3a9a
wangzd <770512850@qq.com>
2018-12-29 17:34:49 +0800

    BM

--- src/main/java/string/Simple.java
+++ src/main/java/string/Simple.java
@@ -5,10 +5,12 @@ package string;
  */
 public class Simple {
     public static void main(String[] args) {
-        System.out.printf("" + kmp("bgooglchegolc", "lc"));
+        System.out.printf("" + kmp("googlehegolc", "golc"));
     }
 
     /**
+     * 朴素算法
+     *
      * @param T ---n
      * @param P ---m
      * @return (n - m + 1)*m
@@ -33,6 +35,8 @@ public class Simple {
     }
 
     /**
+     * KMP算法
+     *
      * @param T ---n
      * @param P ---m
      * @return O(n + m)
@@ -70,6 +74,31 @@ public class Simple {
             }
         }
         return next;
+    }
+
+    /**
+     * BM算法
+     *
+     * @param T
+     * @param P
+     * @return
+     */
+    public static int bm(String T, String P) {
+        int n = T.length();
+        int m = P.length();
+        int skip = 0;
+        int[] bad=getBad();
+        for (int i = 0; i < n - m; i += skip) {
+            skip=0;
+            for (int j = 0; j >=0; j++) {
+
+            }
+        }
+        return 0;
+    }
+
+    private static int[] getBad() {
+        return new int[0];
     }
 
 }
