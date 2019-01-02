6d8103c52ef95fe43727f88685920944d28f9249
wangzd <770512850@qq.com>
2019-01-02 17:42:12 +0800

    无向图图的DFS

--- src/main/java/lettcode/Q100/Q14.java
+++ src/main/java/lettcode/Q100/Q14.java
@@ -1,41 +1,26 @@
 package lettcode.Q100;
 
+import java.util.LinkedList;
+
 /**
- * Created by Administrator on 2018/5/17.
- * 最长公共前缀
+ * 14. 最长公共前缀
+ * https://leetcode-cn.com/problems/longest-common-prefix/
  */
 public class Q14 {
     public static void main(String[] args) {
-        String[] s = {"flower","fsow","flight"};
-        String q=longestCommonPrefix(s);
+        String[] s = {"flower", "fsow", "flight"};
+        String q = longestCommonPrefix(s);
         System.out.print(q);
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        int min = Integer.MAX_VALUE;
-        int index = 0;
-        for (int i = 0; i < strs.length; i++) {
-            if (strs[i].length() < min) {
-                min = strs[i].length();
-                index = i;
-            }
-        }
-        String minStr=strs[index];
-        for (int i = 0; i < minStr.length(); i++) {
-            String match=".*"+minStr.substring(0,minStr.length()-i)+".*";
-            boolean bMatch=false;
-            for (int j = 0; j < strs.length; j++) {
-                if(strs[j].matches(match)){
-                    bMatch=true;
-                }else{
-                    bMatch=false;
-                    break;
-                }
-            }
-            if(bMatch){
-                match=match.substring(2,match.length()-2);
-                return match;
-            }
+        StringBuilder sb = new StringBuilder();
+        LinkedList linkedList = new LinkedList();
+        int i = 0, j = 0;
+        while (true) {
+            String s = strs[i];
+            linkedList.push("");
+            break;
         }
         return "";
     }
