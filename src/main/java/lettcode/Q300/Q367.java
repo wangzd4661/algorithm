4d18d99932521c7ee7f936c71a8802ef3e801082
wangzd4661 <wangzd4661@gmail.com>
2020-03-25 09:43:15 +0800

    Q367

---
+++ src/main/java/lettcode/Q300/Q367.java
@@ -0,0 +1,26 @@
+package lettcode.Q300;
+
+public class Q367 {
+    public static void main(String[] args) {
+        System.out.printf(""+isPerfectSquare(16));
+    }
+    public static boolean isPerfectSquare(int num) {
+        if (num < 2) {
+            return true;
+        }
+        long left = 2, right = num / 2, x, guessSquared;
+        while (left <= right) {
+            x = left + (right - left) / 2;
+            guessSquared = x * x;
+            if (guessSquared == num) {
+                return true;
+            }
+            if (guessSquared > num) {
+                right = x - 1;
+            } else {
+                left = x + 1;
+            }
+        }
+        return false;
+    }
+}
