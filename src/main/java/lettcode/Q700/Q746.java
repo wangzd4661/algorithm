2f1b788d115245a2522da884b27ee4e3185242d3
wangzd4661 <wangzd4661@gmail.com>
2021-12-15 16:50:30 +0800

    746

--- src/main/java/lettcode/Q700/Q746.java
+++ src/main/java/lettcode/Q700/Q746.java
@@ -8,15 +8,24 @@ package lettcode.Q700;
 public class Q746 {
     public static void main(String[] args) {
         int[] n = {10, 15, 20};
-        System.out.printf("" + minCostClimbingStairs(n));
+        System.out.printf("" + minCostClimbingStairs2(n));
     }
 
     public static int minCostClimbingStairs(int[] cost) {
         int[] result = new int[cost.length + 1];
-        for (int i = 2; i < cost.length; i++) {
-            result[i] = Math.min(result[i - 1] + cost[i], result[i - 1]);
+        for (int i = 2; i <= cost.length; i++) {
+            result[i] = Math.min(result[i - 1] + cost[i-1], result[i - 2]+ cost[i-2]);
         }
-        return result[cost.length - 1];
+        return result[cost.length];
+    }
+    public static int minCostClimbingStairs2(int[] cost) {
+        int pre=0,cur=0;
+        for (int i = 2; i <=cost.length; i++) {
+            int min=Math.min(cur+cost[i-1],pre+cost[i-2]);
+            pre=cur;
+            cur=min;
+        }
+        return cur;
     }
 
 }
